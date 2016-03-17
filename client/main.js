898c129b9a35800ca0db536338a6edf8e2236a87
Oscar Dulzaides <tuti@iMac.local>
2016-03-16 18:24:05 -0700

    Routing to comments page added. Styling not done yet.

--- client/main.js
+++ client/main.js
@@ -1,134 +1,182 @@
+/// routing
+Router.configure({
+    'layoutTemplate': 'app_layout'
+}); ///App Layout
 
-    Session.set("websiteLimit", 8);
-
-
-    lastScrollTop = 0;
-    $(window).scroll(function (event) {
-
-        // test if we are near the bottom of the window
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-            // where are we in the page?
-            var scrollTop = $(this).scrollTop();
-            // test if we are going down
-            if (scrollTop > lastScrollTop) {
-                // yes we are heading down...
-                console.log("We are heading down");
-                Session.set("websiteLimit", Session.get("websiteLimit") + 4);
-            }
-
-            lastScrollTop = scrollTop;
-        }
-
-    })
-
-    Accounts.ui.config({
-        passwordSignupFields: "USERNAME_AND_EMAIL"
+/// home '/'
+Router.route('/', function() {
+    this.render('nav', {
+        to: "navbar"
     });
-
-
-    /////
-    // template helpers
-    /////
-
-
-    // helper function that returns all available websites
-    Template.website_list.helpers({
-        websites: function () {
-            return Websites.find({},{sort:{upvote:-1}});
-        },
-
+    this.render('welcome', {
+        to: "main"
     });
+});
 
-    Template.body.helpers({
-        username: function () {
-            if (Meteor.user()) {
-                return Meteor.user().username;
-                //return Meteor.user().emails[0].address;
+/// Website List
+Router.route('/website_list', function() {
+    this.render('nav', {
+        to: "navbar"
+    });
+    this.render('website_main_list', {
+        to: "main"
+    });
+});
 
-            }
-            else {
-                return "anonymous internet user";
-            }
+/// Website Detail
+Router.route('/comments/:_id', function() {
+    this.render('nav', {
+        to: "navbar"
+    });
+    this.render('comments', {
+          to: "main",   
+        data:function(){
+      console.log(Websites.findOne({_id:this.params._id}));
+      return Websites.findOne({_id:this.params._id});
         }
     });
-    Template.website_item.helpers({
-        getUser:function(user_id){
-            var user = Meteor.users.findOne({_id:user_id});
-            // console.log(user);
-            if(user){
-                return user.username;
-            } else {
-                return "Anonymous internet user"
-            }
+});
+
+///     infiniscroll
+
+Session.set("websiteLimit", 8);
+
+
+lastScrollTop = 0;
+$(window).scroll(function(event) {
+
+    /// test if we are near the bottom of the window
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+        // where are we in the page?
+        var scrollTop = $(this).scrollTop();
+        // test if we are going down
+        if (scrollTop > lastScrollTop) {
+            // yes we are heading down...
+            console.log("We are heading down");
+            Session.set("websiteLimit", Session.get("websiteLimit") + 4);
         }
-    });
 
- /////
-    // template events
-    /////
+        lastScrollTop = scrollTop;
+    }
 
-    Template.website_item.events({
-        "click .js-upvote": function (event) {
-            // example of how you can access the id for the website in the database
-            // (this is the data context for the template)
-            var website_id = this._id;
-            console.log("Up voting website with id " + website_id);
-            //
-            // TODO - put the code in here to add a vote to a website!
-            Websites.update({_id: website_id},{$inc: {upvote: +1}}); // for a vote UP
+});
 
-            return false;// prevent the button from reloading the page
-        },
-        "click .js-downvote": function (event) {
+/// account ui setup
 
-            // example of how you can access the id for the website in the database
-            // (this is the data context for the template)
-            var website_id = this._id;
-            console.log("Down voting website with id " + website_id);
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+});
 
-            // TODO - put the code in here to remove a vote from a website!
-            Websites.update({_id: website_id},{$inc: {downvote: +1}});
+///     template helpers
 
-            return false;// prevent the button from reloading the page
+///     helper function that returns all available websites
+Template.website_list.helpers({
+    websites: function() {
+        return Websites.find({}, { sort: { upvote: -1 } });
+    },
+
+});
+
+Template.body.helpers({
+    username: function() {
+        if (Meteor.user()) {
+            return Meteor.user().username;
+            //return Meteor.user().emails[0].address;
+
+        } else {
+            return "anonymous internet user";
         }
-    })
+    }
+});
+Template.website_item.helpers({
+    getUser: function(user_id) {
+        var user = Meteor.users.findOne({ _id: user_id });
+        // console.log(user);
+        if (user) {
+            return user.username;
+        } else {
+            return "Anonymous internet user";
+        }
+    }
+});
+    Template.comments.helpers({
+    getUser: function(user_id) {
+        var user = Meteor.users.findOne({ _id: user_id });
+        console.log(Meteor.users.findOne({_id:user_id}));
+        console.log(user_id);
+        // console.log(user);
+        if (user) {
+            return user.username;
+        } else {
+            return "Anonymous internet user";
+        }
+    },
+});
 
-    Template.website_form.events({
-        "click .js-toggle-website-form": function (event) {
-            $("#website_form").toggle('slow');
-        },
-        "submit .js-save-website-form": function (event) {
+///     template events
 
-            // here is an example of how to get the url out of the form:
-            var url = event.target.url.value;
-            var title = event.target.title.value;
-            var description = event.target.description.value;
-            console.log("The url they entered is: " + url);
+Template.website_item.events({
+    "click .js-upvote": function(event) {
+        // example of how you can access the id for the website in the database
+        // (this is the data context for the template)
+        var website_id = this._id;
+        console.log("Up voting website with id " + website_id);
+        //
+        // TODO - put the code in here to add a vote to a website!
+        Websites.update({ _id: website_id }, { $inc: { upvote: +1 } }); // for a vote UP
 
-            //  TODO - put your website saving code in here!
-            if (Meteor.user()){
-                var date = new Date();
-                Websites.insert({
-                    url:url,
-                    title:title,
-                    description:description,
-                    createdOn:date,
-                    createdBy:Meteor.user()._id,
-                    upVote:0,
-                    downVote:0
-                });
+        return false; // prevent the button from reloading the page
+    },
+    "click .js-downvote": function(event) {
 
-                
-                $("#url").val(" ");
-                $("#title").val(" ");
-                $("#description").val(" ");
-                $("#website_form").toggle('slow');
+        // example of how you can access the id for the website in the database
+        // (this is the data context for the template)
+        var website_id = this._id;
+        console.log("Down voting website with id " + website_id);
 
-                return false;
+        // TODO - put the code in here to remove a vote from a website!
+        Websites.update({ _id: website_id }, { $inc: { downvote: +1 } });
 
-            }
+        return false; // prevent the button from reloading the page
+    }
+})
 
-            return false;// stop the form submit from reloading the page
+Template.website_form.events({
+    "click .js-toggle-website-form": function(event) {
+        $("#website_form").toggle('slow');
+    },
+    "submit .js-save-website-form": function(event) {
+
+        // here is an example of how to get the url out of the form:
+        var url = event.target.url.value;
+        var title = event.target.title.value;
+        var description = event.target.description.value;
+        console.log("The url they entered is: " + url);
+
+        //  TODO - put your website saving code in here!
+        if (Meteor.user()) {
+            var date = new Date();
+            Websites.insert({
+                url: url,
+                title: title,
+                description: description,
+                createdOn: date,
+                createdBy: Meteor.user()._id,
+                upVote: 0,
+                downVote: 0
+            });
+
+
+            // $("#url").val(" ");
+            // $("#title").val(" ");
+            // $("#description").val(" ");
+            // $("#website_form").toggle('slow');
+
+            return false;
 
         }
-    });
+
+        return false; // stop the form submit from reloading the page
+
+    }
+});
